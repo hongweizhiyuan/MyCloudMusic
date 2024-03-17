dd6e9f322d46e25b7ff9dd20d49f426451376f40
YangHongwei <18903467858@qq.com>
2024-03-17 10:31:00 +0800

    feat: 页面跳转

---
+++ MyCloudMusic/Controller/SplashController.h
@@ -0,0 +1,16 @@
+//
+//  SplashController.h
+//  MyCloudMusic
+//
+//  Created by mac on 2024/3/15.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SplashController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
